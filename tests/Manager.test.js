ac2a758702f64fc57c0898a759cea57455fb2a23
Carly Peyton <carlypeyton@gmail.com>
2021-02-14 17:23:22 -0500

    Add manager tests. All passed.

---
+++ tests/Manager.test.js
@@ -0,0 +1,20 @@
+const Manager = require('../lib/Manager');
+// Interns have latest school info displayed
+// Set school for testing
+test("Should set office number", () => {
+    const testValue = "123";
+    const testManager = new Manager("Poe", 13, "poe@test.com", testValue);
+    expect(testManager.office).toBe(testValue);
+});
+
+test("Should get office number via getOfficeNumber() method", () => {
+    const testValue = "123";
+    const testManager = new Manager("Poe", 13, "poe@test.com", testValue);
+    expect(testManager.getOfficeNumber()).toBe(testValue);
+});
+
+test("Should return 'Manager' role via getRole() method", () => {
+    const testValue = "Manager";
+    const testManager = new Manager("Poe", 13, "poe@test.com", "123");
+    expect(testManager.getRole()).toBe(testValue);
+});
