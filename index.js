3e0241e79b5ee8ad85830f13c89d3e21bf22a762
Carly Peyton <carlypeyton@gmail.com>
2021-02-14 15:33:22 -0500

    Create function to add new employee.

--- index.js
+++ index.js
@@ -7,3 +7,33 @@ const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
+// Empty array for team members to be added to 
+var myTeam = [];
+// Function to prompt user for team member role
+function addEmployee() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "role",
+            message: "Would you like to add a teammate? Please select role.",
+            choices: ["Intern", "Engineer", "Manager", "View My Team"],
+        },
+    ]).then(function (res) {
+        // Use switch/case to call new employee functions based on user response
+        switch (res.role) {
+            case "Intern":
+                addIntern();
+                break;
+            case "Engineer":
+                addEngineer();
+                break;
+            case "Manager":
+                addManager();
+                break;
+            // Once team is complete, generate HTML
+            case "View My Team":
+                generateHTML();
+                break;
+        };
+    });
+};
